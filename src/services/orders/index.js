495756b24f99acf6846466f560225922c6aac105
Valentina <valentina@oyga.me>
2021-11-20 19:49:28 -0300

    connect to node

--- src/services/orders/index.js
+++ src/services/orders/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export async function getAllOrders() {
-  const { data } = await axios.get('https://polar-reef-34203.herokuapp.com/api/delivery_orders')
+  const { data } = await axios.get('https://kiwibot-challenge-25486.firebaseapp.com/')
   return data
 }
 
 export async function createOrders({ id, creationDate, status, pickup, dropoff, zoneId }) {
   let { pickupLat, pickupLon } = pickup
   let { dropoffLat, dropoffLon } = dropoff
-  const { data } = await axios.post('https://polar-reef-34203.herokuapp.com/api/delivery_orders', { id, creationDate, status, pickupLat, pickupLon, dropoffLat, dropoffLon, zoneId })
+  const { data } = await axios.post('https://kiwibot-challenge-25486.firebaseapp.com/api/delivery_orders', { id, creationDate, status, pickupLat, pickupLon, dropoffLat, dropoffLon, zoneId })
   return data
 }
